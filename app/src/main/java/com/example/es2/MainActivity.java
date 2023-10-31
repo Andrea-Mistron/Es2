7795f91c149756b15f549e97a328be1101ac3497
mistron <andrea.mistron@ptpvenezia.edu.it>
2023-10-31 14:01:31 +0100

    ciao

--- app/src/main/java/com/example/es2/MainActivity.java
+++ app/src/main/java/com/example/es2/MainActivity.java
@@ -2,13 +2,51 @@ package com.example.es2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.text.BreakIterator;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button somma;
+    private Button differenza;
+    private EditText numero1;   //serve per ricevere il risultato
+    private EditText numero2;
+    private TextView risultato; //serve per stampare il risultato a video
+
+    @SuppressLint("MissingInflatedId")
     @Override
-    protected void onCreate(Bundle savedInstanceState)/*creare l'app(activity)*/ {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main); /*il file xml. R è una super classe costruita internamente all' AndroidStudio che contiene una serie di metodi.*/
+        setContentView(R.layout.activity_main);
+
+        Button btnSaluta = (Button)findViewById(R.id.calcola);
+        final TextView txtViewSaluta= (TextView)findViewById(R.id.calcola);
+        btnSaluta.setOnClickListener(new Button.OnClickListener() {
+            public void onClick(View v) {
+                String strNum1 = numero1.getText().toString();
+                String strNum2 = numero2.getText().toString();
+                int n1 = Integer.parseInt(strNum1);
+                int n2 = Integer.parseInt(strNum2);
+                int res = n1 + n2;
+                String strStr = String.valueOf(res);
+                risultato.setText(strStr);
+            }
+        });
+
+
+        somma = (Button) findViewById(R.id.somma);
+        differenza = (Button) findViewById(R.id.differenza);
+        risultato = (TextView)findViewById(R.id.calcola);
+        numero1 = (EditText)findViewById(R.id.num1);
+        numero2 = (EditText)findViewById(R.id.num2);
+
+//capire come agganciare un ascoltatore ad un bottone.
+        //bisogna usare dei "puntatori" per capire cosa fare una volta che un evento avviene (devo capire cosa fare quando vengo cliccato).
     }
 }
